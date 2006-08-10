4190f2d2b80c85df38ff30eaa3586f0c5bcbb633
Nat Budin <natbudin@gmail.com>
2006-08-10 22:07:00 +0000

    An initial Timeline implementation.
    git-svn-id: file:///var/www/svn/solidfuel@24 82778d24-7814-0410-9c76-c11135be47a2

--- Controllers/timelines.py
+++ Controllers/timelines.py
@@ -1,3 +1,34 @@
 class Timeline:
     def __init__(self):
-        self.actions = []
+        self._upcomingActions = []
+        self._currentActions = []
+        self._pastActions = []
+        self._lastUpdate = 0.0
+
+    def addAction(self, action):
+        if action.end() <= self._lastUpdate:
+            self._pastActions.append(action)
+        elif action.start() <= self._lastUpdate:
+            self._currentActions.append(action)
+        else:
+            self._upcomingActions.append(action)
+
+    def removeAction(self, action):
+        for L in (self._pastActions, self._currentActions, self._upcomingActions):
+            if action in L:
+                L.remove(action)
+                return
+
+    def update(self, time):
+        for action in self._upcomingActions:
+            if action.start() <= time:
+                self._upcomingActions.remove(action)
+                self._currentActions.append(action)
+        for action in self._currentActions:
+            action.update(time)
+            if action.end() <= time:
+                self._currentActions.remove(action)
+                self._pastActions.append(action)
+        self._lastUpdate = time
+            
+            
