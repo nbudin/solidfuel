79b9998b04b6f1c9283e1e2606e9a0e4556845b4
Nat Budin <natbudin@gmail.com>
2007-01-13 18:13:44 +0000

    git-svn-id: file:///var/www/svn/solidfuel@42 82778d24-7814-0410-9c76-c11135be47a2

--- Graphics/ThreeDScene.py
+++ Graphics/ThreeDScene.py
@@ -8,8 +8,6 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 class Light(Translator):
-    lightConst = [GL_LIGHT0, GL_LIGHT1, GL_LIGHT2, GL_LIGHT3, GL_LIGHT4, GL_LIGHT5, GL_LIGHT6, GL_LIGHT7]
-
     def __init__(self):
         Translator.__init__(self)
         self._diffuse = 0.25
@@ -17,7 +15,7 @@ class Light(Translator):
         self._specular = 0.0
     
     def draw(self, n):
-        const = Light.lightConst[n]
+        const = GL_LIGHT0 + n
         glEnable(const)
         glLightfv(const, GL_POSITION, (self.x, self.y, self.z, 1.0))
         glLightfv(const, GL_AMBIENT, [self._ambient] * 4)
@@ -41,8 +39,8 @@ class ThreeDScene(Translatable, Node):
 		glMatrixMode(GL_MODELVIEW)
 		
 	def addLight(self, light):
-	    if len(self._lights) > 7:
-	        raise "Can't have more than 8 lights in a scene!"
+	    if len(self._lights) >= GL_MAX_LIGHTS:
+	        raise "Can't have more than %d lights in a scene!" % (GL_MAX_LIGHTS,)
 	    self._lights.append(light)
 	    
 	def removeLight(self, light):
@@ -51,10 +49,12 @@ class ThreeDScene(Translatable, Node):
 	def translate(self):
 		glMatrixMode(GL_PROJECTION)
 		glLoadMatrixd(self._projMatrix)
-
+		
         glPushAttrib(GL_ALL_ATTRIB_BITS)
-        for flag in (GL_DEPTH_TEST, GL_LIGHTING, GL_NORMALIZE, GL_POLYGON_SMOOTH, GL_TEXTURE_2D):
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        for flag in (GL_DEPTH_TEST, GL_LIGHTING, GL_NORMALIZE, GL_BLEND, GL_POLYGON_SMOOTH):
             glEnable(flag)
+        glDisable(GL_TEXTURE_2D) # Model will handle this for us automatically
         glDisable(GL_BLEND)
         glShadeModel(GL_SMOOTH)
         for i in range(len(self._lights)):
@@ -66,7 +66,7 @@ class ThreeDScene(Translatable, Node):
 		glTranslatef(self.cameraX, self.cameraY, self.cameraZ)
 		glRotatef(self.roll, 0, 0, 1)
 		glRotatef(self.pitch, 1, 0, 0)
-		glRotatef(-self.yaw, 0, 1, 0)
+		#glRotatef(-self.yaw, 0, 1, 0)
 		
 	def untranslate(self):
 	    glPopAttrib()
