f7eace43f449e98895de893bb3841a04796e350d
Nat Budin <natbudin@gmail.com>
2007-02-03 15:41:36 +0000

    git-svn-id: file:///var/www/svn/solidfuel@59 82778d24-7814-0410-9c76-c11135be47a2

---
+++ Logic/EnumField.py
@@ -0,0 +1,36 @@
+from DataField import DataField
+import types
+
+class EnumField(DataField):
+    def __init__(self, codes, value=None):
+        if type(codes) is types.DictType:
+            self._encode = codes
+            self._decode = {}
+            for name in self._encode.keys():
+                self._decode[self._encode[name]] = name
+        else:
+            self._encode = {}
+            self._decode = {}
+            for i in range(len(codes)):
+                self._encode[codes[i]] = i
+                self._decode[i] = codes[i]
+        DataField.__init__(self, self.encode(value))
+        
+    def encode(self, value):
+        if self._encode.has_key(value):
+            return self._encode[value]
+        return None
+        
+    def decode(self, value):
+        if self._decode.has_key(value):
+            return self._decode[value]
+        return None
+        
+    def set(self, value):
+        return DataField.set(self, self.encode(value))
+        
+    def get(self):
+        return self.decode(DataField.get(self))
+    
+    def equals(self, value):
+        return DataField.equals(self, self.encode(value))
--- Logic/__init__.py
+++ Logic/__init__.py
@@ -4,4 +4,5 @@ from ConditionalEvent import *
 from EventGroup import *
 from DataField import *
 from NumberField import *
+from EnumField import *
 from List import *
