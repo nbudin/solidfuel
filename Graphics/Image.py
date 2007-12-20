bd4a735b3356c89f51b8c3fa68338f2cb16898ec
Nat Budin <natbudin@gmail.com>
2007-12-20 22:35:07 +0000

    FEH support
    git-svn-id: file:///var/www/svn/solidfuel@82 82778d24-7814-0410-9c76-c11135be47a2

--- Graphics/Image.py
+++ Graphics/Image.py
@@ -10,28 +10,28 @@ from OpenGL.GL.EXT.texture_filter_anisotropic import *
 
 class Image:
     nextTextures = None
-	def __init__(self, filename):
-		self._texture = None    
-		surf = pygame.image.load(filename)
-		self.initFromSurface(surf)
-		
-	def initFromSurface(self, surf):
-		self.nativeW = self.w = surf.get_width()
-		self.nativeH = self.h = surf.get_height()
+    def __init__(self, f, filenamehint = None):
+        self._texture = None
+        surf = pygame.image.load(f, filenamehint)
+        self.initFromSurface(surf)
 
-		texdata = pygame.image.tostring(surf, "RGBA", 1)
+    def initFromSurface(self, surf):
+        self.nativeW = self.w = surf.get_width()
+        self.nativeH = self.h = surf.get_height()
 
-		if Image.nextTextures is None:
-			Image.nextTextures = list(glGenTextures(1000))
-		self._texture = Image.nextTextures.pop()
-		glBindTexture(GL_TEXTURE_2D, self._texture)
-		if config.use_anisotropic:
-			try:
-				glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 2.0)
-			except:
-				config.use_anisotropic = 0
-		if not config.use_anisotropic:
-			glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
-			glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
-		gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, surf.get_width(), surf.get_height(), 
-			GL_RGBA, GL_UNSIGNED_BYTE, texdata)
+        texdata = pygame.image.tostring(surf, "RGBA", 1)
+
+        if Image.nextTextures is None:
+            Image.nextTextures = list(glGenTextures(1000))
+        self._texture = Image.nextTextures.pop()
+        glBindTexture(GL_TEXTURE_2D, self._texture)
+        if config.use_anisotropic:
+            try:
+                glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, 2.0)
+            except:
+                config.use_anisotropic = 0
+        if not config.use_anisotropic:
+            glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR)
+            glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR)
+        gluBuild2DMipmaps(GL_TEXTURE_2D, GL_RGBA, surf.get_width(), surf.get_height(),
+            GL_RGBA, GL_UNSIGNED_BYTE, texdata)
--- Graphics/Model.py
+++ Graphics/Model.py
@@ -10,22 +10,25 @@ class Model(VisibleNode, Translator):
     texcache = {}
     dlcache = {}
     bbcache = {}
-    
-    def __init__(self, filename, compile=True):
+
+    def __init__(self, filename, compile=True, buffer=None):
         VisibleNode.__init__(self)
         Translator.__init__(self)
-        
+
         self.rotX = 0.0
         self.rotY = 0.0
         self.rotZ = 0.0
-        
+
         self.scale = 1.0
-        
+
         if Model.dlcache.has_key(filename):
             self._displayList = Model.dlcache[filename]
             bb = Model.bbcache[filename]
         else:
-            self._dom = dom3ds.read_3ds_file(filename)
+            if buffer is None:
+                self._dom = dom3ds.read_3ds_file(filename)
+            else:
+                self._dom = dom3ds.read_3ds_mem(buffer)
             self._model = glmodel.GLModel(self._dom, self._loadTexture)
             bb = self._model.bounding_box()
             Model.bbcache[filename] = bb
@@ -36,7 +39,7 @@ class Model(VisibleNode, Translator):
                 self._displayList = None
         self._center = (bb[0] + bb[1]) / 2
         self._modelScale = 1.0 / max(abs(bb[0] - bb[1]))
-    
+
     def translate(self):
         glPushMatrix()
         Translator.translate(self)
@@ -46,20 +49,20 @@ class Model(VisibleNode, Translator):
         glRotate(self.rotX, 1, 0, 0)
         glRotate(self.rotY, 0, 1, 0)
         glRotate(self.rotZ, 0, 0, 1)
-        
+
     def untranslate(self):
         glPopMatrix()
-		
+
     def draw(self):
         if self._displayList is not None:
             glCallList(self._displayList)
         else:
             self._model.render()
-            
+
     def _loadTexture(self, filename):
         if Model.texcache.has_key(filename):
             return Model.texcache[filename]
-        
+
         tex = gltexture.Texture(filename)
         Model.texcache[texfilename] = tex
         return tex
