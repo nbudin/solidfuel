8d6586471f1582c681f31899820226e62836cd77
Nat Budin <natbudin@gmail.com>
2008-01-25 17:07:23 +0000

    Fix typecasting bug in the scale functions
    git-svn-id: file:///var/www/svn/solidfuel@83 82778d24-7814-0410-9c76-c11135be47a2

--- Graphics/Box.py
+++ Graphics/Box.py
@@ -3,37 +3,37 @@ from Node import Node
 import pygame
 
 class Box(Node, Translator):
-	def __init__(self, x=0.0, y=0.0, w=0.0, h=0.0):
-		Translator.__init__(self)
-		Node.__init__(self)
-		self.x = x
-		self.y = y
-		self.w = w
-		self.h = h
-		
-	def aspectRatio(self):
-		return self.w / self.h
+        def __init__(self, x=0.0, y=0.0, w=0.0, h=0.0):
+                Translator.__init__(self)
+                Node.__init__(self)
+                self.x = x
+                self.y = y
+                self.w = w
+                self.h = h
 
-	def scale(self, factor):
-		self.w *= factor
-		self.h *= factor
+        def aspectRatio(self):
+                return float(self.w) / float(self.h)
 
-	def scaleW(self, w):
-		ar = self.aspectRatio()
-		self.w = w
-		self.h = w / ar
+        def scale(self, factor):
+                self.w *= factor
+                self.h *= factor
 
-	def scaleH(self, h):
-		ar = self.aspectRatio()
-		self.h = h
-		self.w = h * ar
+        def scaleW(self, w):
+                ar = self.aspectRatio()
+                self.w = w
+                self.h = w / ar
 
-	def setCenter(self, center):
-		self.x = center[0] - self.w / 2
-		self.y = center[1] - self.h / 2
+        def scaleH(self, h):
+                ar = self.aspectRatio()
+                self.h = h
+                self.w = h * ar
 
-	def getCenter(self):
-		return (self.x + self.w / 2, self.y + self.h / 2)
-		
-	def getCenterRel(self):
-	    return (self.w / 2, self.h / 2)
+        def setCenter(self, center):
+                self.x = center[0] - self.w / 2
+                self.y = center[1] - self.h / 2
+
+        def getCenter(self):
+                return (self.x + self.w / 2.0, self.y + self.h / 2.0)
+
+        def getCenterRel(self):
+            return (self.w / 2.0, self.h / 2.0)
