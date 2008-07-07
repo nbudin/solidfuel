07dab155057d9b564eb2dd9efc30e21380032c68
Nat Budin <natbudin@gmail.com>
2008-07-07 16:48:06 +0000

    port to numpy
    git-svn-id: file:///var/www/svn/solidfuel@85 82778d24-7814-0410-9c76-c11135be47a2

--- Graphics/Billboard.py
+++ Graphics/Billboard.py
@@ -1,20 +1,20 @@
 from Sprite import Sprite
-from Numeric import *
+from numpy import *
 from OpenGL.GL import *
 import math
 
 class Billboard(Sprite):
     def _normalize(self, v):
         return v  / sqrt(add.reduce(v * v))
-    
+
     def _cross(self, b, c):
         return array([b[1] * c[2] - c[1] * b[2],
             b[2] * c[0] - c[2] * b[0],
             b[0] * c[1] - c[0] * b[1]], 'd')
-    
+
     def translate(self):
         Sprite.translate(self)
-        
+
         glPushMatrix()
         # This assumes that the camera is at (0, 0, 0)!  Really this is not a good assumption.
         objToCamProj = self._normalize(array([-self.x, 0, -self.z], 'd'))
@@ -23,16 +23,16 @@ class Billboard(Sprite):
         angleCosine = add.reduce(lookAt * objToCamProj)
         if angleCosine < 0.99990 and angleCosine > -0.9999:
             glRotatef(math.acos(angleCosine) * 180 / 3.14, *upAux)
-        
+
         objToCam = self._normalize(array([-self.x, -self.y, -self.z], 'd'))
         angleCosine = add.reduce(objToCamProj * objToCam)
         if angleCosine < 0.99990 and angleCosine > -0.9999:
-            glRotatef(acos(angleCosine)*180/3.14, 1, 0, 0)
+            glRotatef(math.acos(angleCosine)*180/3.14, 1, 0, 0)
         else:
-            glRotatef(acos(angleCosine)*180/3.14, -1, 0, 0)
-        
+            glRotatef(math.acos(angleCosine)*180/3.14, -1, 0, 0)
+
     def untranslate(self):
         glPopMatrix()
         Sprite.untranslate(self)
-        
-        
+
+
