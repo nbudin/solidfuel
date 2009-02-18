8cfbc848047f48fe1fd01bb08a735ffd80ff7792
Nat Budin <natbudin@gmail.com>
2009-02-18 19:57:43 +0000

    Move everything under solidfuel/
    git-svn-id: file:///var/www/svn/solidfuel@94 82778d24-7814-0410-9c76-c11135be47a2

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Pyrex.Distutils import build_ext
+#from Pyrex.Distutils import build_ext
 setup(
   name = "solidfuel",
   description = "Multimedia framework for Python",
   author = "Nat Budin",
   author_email = "natbudin@gmail.com",
-  packages = ['Graphics', 'Logic'],
-  ext_modules=[
-    Extension("Graphics.Node", ["Graphics/Node.pyx"], libraries = [])
-    ],
-  cmdclass = {'build_ext': build_ext}
+  packages = ['solidfuel', 'solidfuel.Graphics', 'solidfuel.Logic',
+              'solidfuel.Controllers', 'solidfuel.Gui', 'solidfuel.Math',
+             ]
+  #ext_modules=[
+  #  Extension("Graphics.Node", ["Graphics/Node.pyx"], libraries = [])
+  #  ],
+  #cmdclass = {'build_ext': build_ext}
 )
