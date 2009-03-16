5c40e1e0039b5d7c5a9d0f6be053e3bbcb86da0c
Nat Budin <natbudin@gmail.com>
2009-03-16 11:52:41 +0000

    Allow sized points
    git-svn-id: file:///var/www/svn/solidfuel@100 82778d24-7814-0410-9c76-c11135be47a2

--- solidfuel/Graphics/Point.py
+++ solidfuel/Graphics/Point.py
@@ -10,11 +10,16 @@ class Point(VisibleNode, Translator):
         Translator.__init__(self)
         self.color = (1.0, 1.0, 1.0)
         self.opacity = 1.0
+        self.size = None
     
     def draw(self):
         glEnable(GL_POINT_SMOOTH)
 		glHint(GL_POINT_SMOOTH_HINT, GL_NICEST)
         glColor(self.color[0], self.color[1], self.color[2], self.opacity)
+        if self.size:
+            glPointSize(self.size)
 		glBegin(GL_POINTS)
         glVertex3f(0.0, 0.0, 0.0)
         glEnd()
+        if self.size:
+            glPointSize(1.0)
